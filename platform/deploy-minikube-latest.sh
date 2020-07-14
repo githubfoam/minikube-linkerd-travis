e74dec5904dccabce89003626622ac4f2a0c23bf
githubfoam <lequestionis@gmail.com>
2020-07-14 16:18:37 +0300

    сам не свой

--- platform/deploy-minikube-latest.sh
+++ platform/deploy-minikube-latest.sh
@@ -27,7 +27,13 @@ mkdir -p $HOME/.kube $HOME/.minikube
 minikube start --profile=minikube --vm-driver=none #the none driver, the kubectl config and credentials generated are owned by root in the root user’s home directory
 minikube status #* There is no local cluster named "minikube"
 minikube update-context --profile=minikube
-`chown -R travis: /home/travis/.minikube/`
+
+# `chown -R travis: /home/travis/.minikube/`
+echo $USER
+echo $HOME 
+`chown -R $USER: $HOME/.minikube/`
+
+
 eval "$(minikube docker-env --profile=minikube)" && export DOCKER_CLI='docker'
 
 echo "=========================================================================================="
